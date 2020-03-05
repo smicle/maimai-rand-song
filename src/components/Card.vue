3d68f2a254b0a9fa75a30fbdc0239f7f804c86cb
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-03-06 02:31:00 +0900

    Add レイアウトを作成

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,19 @@
 <template>
   <v-card>
-    <v-card-title :class="title">
+    <v-card-title class="caption pa-2">
       <slot name="title"></slot>
     </v-card-title>
-    <v-card-text :class="text" :style="bl">
+    <v-card-text class="body-2" :style="bl">
       <slot name="text"></slot>
     </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
-import {Component, Vue, Prop} from 'vue-property-decorator'
+import {Component, Vue} from 'vue-property-decorator'
 @Component
 export default class IdolCard extends Vue {
   private bl = 'color: rgba(0, 0, 0, 0.87);'
-
-  @Prop()
-  private title?: string
-
-  @Prop()
-  private text?: string
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,42 +3,77 @@
     <h1 class="font-weight-medium">おまかせセレクト</h1>
 
     <v-row>
-      <v-col cols="7" class="pr-0">
-        <Card class="ml-4 mr-2" title="caption">
+      <v-col cols="12">
+        <v-expansion-panels :focusable="true" class="px-4">
+          <v-expansion-panel class="">
+            <v-expansion-panel-header>フィルター</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              まだ実装してない。
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
+
+    <v-row class="mx-3">
+      <v-col cols="8" md="4" class="pa-1">
+        <Card class="mx-0">
           <template v-slot:title>ジャンル</template>
           <template v-slot:text>{{ song.genre }}</template>
         </Card>
       </v-col>
 
-      <v-col cols="2" class="px-0">
-        <Card title="caption" text="caption">
-          <template v-slot:title>形式</template>
-          <template v-slot:text>{{ song.format }}</template>
-        </Card>
-      </v-col>
-
-      <v-col cols="3" class="pl-0">
-        <Card class="mx-2" title="caption" text="caption">
+      <v-col cols="4" md="2" class="pa-1">
+        <Card class="mx-0">
           <template v-slot:title>Lv</template>
           <template v-slot:text>{{ song.level }}</template>
         </Card>
       </v-col>
-    </v-row>
 
-    <v-row :style="H(160)">
-      <v-col cols="8" class="pr-0">
-        <Card class="ma-4 mt-0 mr-0" title="body-2" text="body-1">
-          <template v-slot:title>楽曲</template>
-          <template v-slot:text>{{ song.title }}</template>
+      <v-col cols="8" md="4" class="pa-1">
+        <Card class="mx-0">
+          <template v-slot:title>バージョン</template>
+          <template v-slot:text>{{ song.version }}</template>
         </Card>
       </v-col>
 
-      <v-col cols="4" class="pl-0">
-        <img :src="song.src" :style="H(92)" />
+      <v-col cols="4" md="2" class="pa-1">
+        <Card class="mx-0">
+          <template v-slot:title>形式</template>
+          <template v-slot:text>{{ song.format }}</template>
+        </Card>
       </v-col>
     </v-row>
 
-    <v-btn @click="result" color="info" class="display-1	" :style="H(120) + W(250)">次へ</v-btn>
+    <v-row :style="H(150)" class="pa-3">
+      <v-col cols="12">
+        <v-card class="d-flex mx-0">
+          <div>
+            <v-avatar tile class="" size="92">
+              <img :src="song.src" />
+            </v-avatar>
+          </div>
+
+          <div style="width: 100%">
+            <v-card-title class="caption pa-2 pb-0">楽曲</v-card-title>
+            <div style="position:relative" :style="H(56)">
+              <v-card-text
+                class="body-2"
+                style="
+                  color: rgba(0, 0, 0, 0.87);
+                  position:absolute;
+                  top: 50%;
+                  transform: translateY(-50%)
+                "
+                >{{ song.title }}</v-card-text
+              >
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-btn @click="result" color="info" class="display-1" :style="H(120) + W(250)">次へ</v-btn>
 
     <div class="d-flex flex-row-reverse pt-2 pr-4 mt-4">
       <v-btn to="/about" text class="caption" color="blue darken-3" height="20" width="70">
@@ -56,9 +91,9 @@ import maimai from '@/json/maimai.json'
 interface Song {
   title: string
   genre: string
+  version: string
   level: string
   format: string
-  DXscore: string
   src: string
 }
 
@@ -77,9 +112,9 @@ export default class Home extends Mixins(MixinStyle) {
   private song: Song = {
     title: 'ネコ日和。',
     genre: 'maimai',
+    version: 'maimai',
     level: '10+',
     format: 'Std',
-    DXscore: '1038',
     src: require('@/assets/img/maimai/ネコ日和。.jpg'),
   }
 
@@ -87,14 +122,16 @@ export default class Home extends Mixins(MixinStyle) {
     const rand: number = await require('random-number-csprng')(0, maimai.length)
     const song = maimai.splice(rand, 1)[0]
 
+    if (song === undefined) return
+
     const rep = (title: string): string => title.replace(/\/|"|’|“|”|>|:|♡|!|é|&|✪|♥|－|♣|⤴/g, '')
 
     this.song = {
       title: song.title,
       genre: song.genre,
+      version: song.version,
       level: song.level,
       format: song.format,
-      DXscore: song.DXscore,
       src: require(`@/assets/img/${song.genre.replace('&', '_')}/${rep(song.title)}.jpg`),
     }
   }
